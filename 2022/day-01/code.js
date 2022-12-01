c0c80c6a19d74de97220469f4cefad78bc142301
Marlaqk <ralsch@pm.me>
2022-12-01 07:46:54 +0100

    day-01

--- 2022/day-01/code.js
+++ 2022/day-01/code.js
@@ -1,16 +1,8 @@
 const utils = require('../../utils');
 
-const inventories = [];
-let calories = 0;
-utils.getInput().forEach(line => {
-    if (line == '') {
-        inventories.push(calories);
-        calories = 0;
-    } else {
-        calories += parseInt(line);
-    }
-})
-inventories.push(calories);
-
-console.log(Math.max(...inventories))
-console.log(utils.sum(inventories.sort((a,b) => a - b).slice(-3)))
+let [a,b,c] = utils.getRawInput()
+    .split('\n\n')
+    .map(x => utils.sum(x.split('\n').map(v => +v)))
+    .sort((a,b) => b - a)
+console.log(a)
+console.log(a+b+c)
